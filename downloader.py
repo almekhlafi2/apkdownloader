3a601bc0ff88d5a29e7b6b9a684d386347bc29ef
Tobias Funke <tobiasfunke93@gmail.com>
2019-07-17 12:26:22 +0200

    add captcha example, versions, variants

--- downloader.py
+++ downloader.py
@@ -9,11 +9,22 @@
 import os, sys
 import requests as req
 from lxml import html
+import svglib
 
 host = 'https://apkpure.com'
-download_dir = './downloads/'
+download_dir = './downloads'
 apps = []
 count = 0
+
+'''
+captcha = 'https://a.apkpure.com/captcha'
+https://apkpure.com/hospitality-mobile-access/com.assaabloy.hospitality.mobileaccess.hospitalitymobileaccess
+https://apkpure.com/hospitality-mobile-access/com.assaabloy.hospitality.mobileaccess.hospitalitymobileaccess/download?from=details
+https://apkpure.com/hospitality-mobile-access/com.assaabloy.hospitality.mobileaccess.hospitalitymobileaccess/versions
+https://apkpure.com/hospitality-mobile-access/com.assaabloy.hospitality.mobileaccess.hospitalitymobileaccess/download/3216-APK?from=versions%2Fversion
+https://apkpure.com/hospitality-mobile-access/com.assaabloy.hospitality.mobileaccess.hospitalitymobileaccess/download/3175-APK?from=versions%2Fversion
+<div class="ver">
+'''
 
 s = req.Session()
 
@@ -40,7 +51,7 @@ def search(searchstring):
         pass
     
     for app in list(zip(titles, links, icons)):
-        apps.append({'title':app[0], 'link':host + app[1], 'icon':app[2], 'downloadpage':'', 'version':'', 'downloadlink':'', 'filename':'', 'package':app[1].split('/')[2]})
+        apps.append({'title':app[0][:-4], 'link':host + app[1], 'desc':'', 'icon':app[2], 'downloadpage':'','latest_version':'', 'versions':[], 'filename':'', 'package':app[1].split('/')[2]})
 
 
 def details(app):
@@ -52,33 +63,43 @@ def details(app):
     tree = html.fromstring(response.content)
     
     downloadpage = tree.xpath('//div[@class="ny-down"]/a[@class=" da"]/@href')
-    version  = tree.xpath('//div[@class="details-sdk"]/span/text()')
+    latest_version  = tree.xpath('//div[@class="details-sdk"]/span/text()')
     
     if len(downloadpage) == 0:
         downloadpage = tree.xpath('//div[@class="ny-down ny-var"]/a[@class=" da"]/@href')
     
     if debug:
         #print(downloadpage)
-        #print(version)
+        #print(latest_version)
         pass
     
     app['downloadpage'] = host + downloadpage[0]
-    app['version'] = version[0].replace(' ','')
+    app['latest_version'] = latest_version[0].replace(' ','')
 
+def versions(app):
+    if app['downloadpage'] == None:
+        app['versions'] = None
+        return 0
+    
+    response = s.get(app['link'] + '/versions')
+    if response.status_code == 404:
+        app['versions'] = []
+        return 0
+    
+    tree = html.fromstring(response.content)
+    
+    download  = tree.xpath('//div[@class="ver"]/ul/li/a/@href')
+    version   = tree.xpath('//div[@class="ver"]/ul/li/a/div/div/span[@class="ver-item-n"]/text()')
+    #variants  = tree.xpath('//div[@class="ver"]/ul/li/a/div/div/span[@class="ver-item-t"]/text()')
+    
+    for v in zip(version, download):
+        app['versions'].append({'version':v[0],'download':host + v[1]})
+    
 
-def download(app):
+def download(app, all_versions=False):
     if app['downloadpage'] == None:
         print('404 status  -', app['package'])
         return '404 status'
-    
-    download_path = download_dir + app['package']
-    
-    if not os.path.exists(download_path):
-        os.makedirs(download_path)
-    
-    download_path += '/' + app['version']
-    if not os.path.exists(download_path):
-        os.makedirs(download_path)
     
     if debug:
         #print(download_path)
@@ -97,6 +118,14 @@ def download(app):
     
     app['filename'] = filename[0].replace(' ','_').replace('–','-')[:-1]
     app['downloadlink'] = downloadlink[0]
+    
+    download_path = download_dir + '/' + app['package']
+    if not os.path.exists(download_path):
+        os.makedirs(download_path)
+    
+    download_path += '/' + app['version']
+    if not os.path.exists(download_path):
+        os.makedirs(download_path)
     
     download_path += '/' + app['filename']
     if os.path.exists(download_path):
@@ -123,11 +152,20 @@ def download(app):
 
 def main():
     search('hospitality'.replace(' ','+'))
-    for i in range(len(apps)):
-        details(apps[i])
     
     for i in range(len(apps)):
-        download(apps[i])
+        details(apps[i])
+        
+    for i in range(len(apps)):
+        versions(apps[i])
+    
+    for i in range(len(apps)):
+        print(apps[i])
+        print()
+        #break
+    
+    #for i in range(len(apps)):
+    #    download(apps[i], all_versions=True)
     
 if __name__== "__main__":
     main()
